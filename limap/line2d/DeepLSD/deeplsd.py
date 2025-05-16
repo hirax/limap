6eaecb4fb1e62fb7225c79ba343403ccc76eb7de
jun hirabayashi <jun.hirabayashi@gmail.com>
2025-05-16 13:34:16 +0900

    Update limap/line2d/DeepLSD/deeplsd.py for PyTorch 2.6

--- limap/line2d/DeepLSD/deeplsd.py
+++ limap/line2d/DeepLSD/deeplsd.py
@@ -33,7 +33,8 @@ class DeepLSDDetector(BaseDetector):
             )
         if not os.path.isfile(ckpt):
             self.download_model(ckpt)
-        ckpt = torch.load(ckpt, map_location="cpu")
+        #ckpt = torch.load(ckpt, map_location="cpu", weights_only=False) # original
+        ckpt = torch.load(ckpt, map_location="cpu", weights_only=False)  #jh: we need this for PyTorch 2.6 
         self.net = DeepLSD(conf).eval()
         self.net.load_state_dict(ckpt["model"])
         self.net = self.net.to(self.device)
